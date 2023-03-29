9f3544ca654977ee2c3fb970b15727ca022dd584
Adam Rathbun <92285612+AdamRathbun@users.noreply.github.com>
2023-03-28 21:32:15 -0400

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,33 +7,21 @@ const router = express.Router();
 //logic under ../controllers/posts.js this just the route. 
 //note urls match client/src/api/index.js
 
-//route for getting posts, need to change from all feed to feed of projects that I'm on. router.get('/:id', getPost) etc.
 router.get('/', auth, getPosts)
 
 //route for creating posts
 router.post('/', auth, createPost)
 
-//route for updating existing posts. note here it's "patch", not "put". managed on frontend.
-// will have logic to only allow updating posts I created. most likely fine to have in addition to common comment objects that allow everyone to comment. id of existing post being updated. 
 router.patch('/:id', auth, updatePost)
 
-//route for deleting posts. managed on frontend.
-// will have logic for only deleting posts I created
+//only delete posts user created
 router.delete('/:id', auth, deletePost)
 
-//route for liking project posts. managed on backend.
 router.patch('/:id/likePost', auth, likePost)
 
-router.get('/:id', getPost)
+router.get('/:id', auth, getPost)
 
-//route for creating comments
 router.post('/:id/commentPost', auth, commentPost)
-
-// route for updating comments
-// router.patch('/:id/updateComment', auth, updateComment)
-
-//route for custom feed
-// router.get('/:id', auth, getFeed)
 
 export default router;
 
