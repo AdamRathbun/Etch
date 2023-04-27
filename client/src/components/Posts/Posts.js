465d8d707cd85ec2e5d36e4cba0a021174e119bb
Adam Rathbun <92285612+AdamRathbun@users.noreply.github.com>
2023-04-26 22:57:43 -0400

    Update Posts.js

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -8,7 +8,6 @@ import useStyles from './styles'
 
 
 const Posts = ({ setCurrentId }) => {
-    //initialize selector as a hook. now can access redux store, i.e. state. it's called state.posts b.c in src/reducers/index.js, it's exported as posts
     const posts = useSelector((state)=>state.posts)
 
     const classes = useStyles()
@@ -18,14 +17,12 @@ const Posts = ({ setCurrentId }) => {
         // if no posts, then a loading spinner circularprogress. if there are posts, then show posts
         !posts.length ? <CircularProgress />: (
             <Grid className={classes.container} container alignItems='stretch' spacing={3}>
-                {/* remember {} is to indicate JS logic */}
-                { Array.from(posts.map((post)=>(
-                    // has a post ID. xs={12} means full width on mobile devices. also note the () instead of {} above
+                {posts.map((post)=>(
+                    // has a post ID. xs={12} means full width on mobile devices. 
                     <Grid key={post._id} item xs={12} sm={12}>
-                        {/* post prop. sends the value of each post to each post component through map method. also is passed setCurrentId */}
                         <Post post={post} setCurrentId={setCurrentId}/>
                     </Grid>
-                ))).reverse() }
+                ))}
             </Grid>
         )
     )
