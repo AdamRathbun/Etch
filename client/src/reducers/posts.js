278a3ed706fe794707fee2be7596902cd2d86a80
Adam Rathbun <92285612+AdamRathbun@users.noreply.github.com>
2023-04-26 22:21:40 -0400

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,12 @@
-//reducer for posts. note I am using this in sibling index.js as combineReducers
-
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_POST} from '../constants/actionTypes.js';
 
 export default (posts=[], action) => {
     switch (action.type){
         case FETCH_ALL:
-            // actions/src/posts.js ln 15
             return action.payload;
         case CREATE:
             return [...posts, action.payload];
         case UPDATE:
-            // map over the posts array with new array. note action.payload is the updated post here.
             return posts.map((post) => post._id === action.payload._id ? action.payload : post)
         case DELETE:
             return posts.filter((post) => post._id !== action.payload);
