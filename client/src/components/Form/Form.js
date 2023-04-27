25eaf430c09b6a749291885982e73aa77f001c7f
Adam Rathbun <92285612+AdamRathbun@users.noreply.github.com>
2023-04-26 22:41:18 -0400

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -13,18 +13,14 @@ const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         title:'', message: '', tags: '', selectedFile: '', comments:[], width: 0, height:0, version: 1,
     })
-    // const [dimensions, setDimensions] = useState({width: 0, height:0})
 
-
-    // for Form. if there's a currentId, this looks for the post with a matching id to currentId, else null. this returns a single post that matches the currentId.
+    // if there's a currentId, this looks for the post with a matching id to currentId, else null. 
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
-    // when the post value changes from nothing to the actual post, run this function. if post exists, then setPostData and populate it with the data of the post.
-    // this combined with above makes it so that when I (click on the elipsis to update), that it auto populates the form with the post data with an id matching the currentId
+
     useEffect(() => {
         if(post) setPostData(post)
     }, [post])
 
-    //dispatch actions
     const dispatch = useDispatch();
     const navigate = useNavigate()
 
@@ -32,14 +28,12 @@ const Form = ({ currentId, setCurrentId }) => {
 
     const user = JSON.parse(localStorage.getItem('profile'))
 
-    //tied to submit button > onSubmit for the form. tied to api/index.js and actions/posts.js
-    //note after action is dispatched, we go to reducers/posts.js
+    // tied to api/index.js and actions/posts.js. note after action is dispatched, we go to reducers/posts.js
     const handleSubmit = (e) => {
         //prevent refresh in browser
         e.preventDefault();
-        // redux. if currentId is known, update. else, create. note currentId is the first parameter of updatePost so it knows which one to update. note clear function called after each submit/update of a post.
+        // redux. if currentId is known, update. else, create. 
         if(currentId===0){
-            // spread out the postData. grabs it from localStorage (see user variable above), then saves the name from the localStorage as the name in the database
             dispatch(createPost({...postData, name: user?.result?.name }));
             clear();
         } else {
@@ -65,17 +59,6 @@ const Form = ({ currentId, setCurrentId }) => {
     setPostData({ title: '', message: '', tags: '', selectedFile: '', width: 0, height:0, version: 1 });
   };
 
-// 9.26
-
-// const getDimensions = (base64) => {
-//     let i = new Image(); 
-
-//     i.onload = function(){
-//      alert( i.width+", "+i.height );
-//     };
-    
-//     i.src = base64;
-// }
 const getDimensions = (base64) => {
     let i = new Image(); 
 
@@ -97,15 +80,13 @@ const getDimensions = (base64) => {
     i.src = base64;
 }
 
-// console.log(postData.width, postData.height)
-
     return (
         //Paper is like a form with a white background
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit} >
                 {/* ternary for displaying Creating vs Editing a project */}
                 <Typography variant='h6'>{currentId ? 'Edit' : 'Create'} a project</Typography>
-                {/* remember the entire data from post stored in the postData object and each of that object is a specific text field. The onChange copies over previous state and only changes creator property's value. can similary do for other properties. it sets the state using an object*/}
+
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e)=>setPostData({ ...postData, title: e.target.value })} />
                 <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e)=>setPostData({ ...postData, message: e.target.value })} />
                 {/* note the tags apply split(). users separate with comma no space */}
@@ -124,8 +105,6 @@ const getDimensions = (base64) => {
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
                 <Typography variant='p' className={classes.instructions} >
-                    {/* Instructions: Login first. Input projects details including image. Click on a project to open it up, with markup tools and comments below the image. Can share project with additional users by entering the ID into "Add User" field. */}
-
                     <strong>Instructions:</strong> Add image to project. Click project to open it with markup tools & comments. Share project by entering friend's ID into "Add User".
                 </Typography>
             </form>
@@ -135,14 +114,3 @@ const getDimensions = (base64) => {
 }
 
 export default Form
-
-
-// more
-
-// var i = new Image(); 
-
-// i.onload = function(){
-//  alert( i.width+", "+i.height );
-// };
-
-// i.src = imageData; 
