ce196b1031e2e368839d23e9aef210fd683c7174
Adam Rathbun <92285612+AdamRathbun@users.noreply.github.com>
2023-04-26 21:58:51 -0400

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,7 +19,7 @@ export const createPost = async (req, res) => {
     }
 }
 
-//update posts. the request is made to the post of the right id (ex. /posts/111 and the 111 would fill the id below)
+//update posts. the request is made to the post of the right id
 export const updatePost = async (req, res) => {
     //desctructuring + renaming id to _id for mongoose id
     const { id: _id } = req.params;
@@ -30,8 +30,7 @@ export const updatePost = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id, bucko.')
 
     //if that post id is valid, then update the post
-    //note the update data would come from req.body from above (note it's data from frontend). new: true so it gets the updated version of the post.
-    //also updated findByIdAndUpdate(_id, post, { new: true } to current
+    //note the update data would come from req.body from above (note it's data from frontend)
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
     
     res.json(updatedPost)
@@ -53,25 +52,20 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params
 
-    // see ln17 of ../middleware/auth.js
     if(!req.userId) return res.json({message: 'Unauthenticated.'})
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id.')
 
-    //returns a post matching the id
     const post = await PostMessage.findById(id)
 
-    // loop through each post's likes to see userIds and if person has already liked
+    // check if person has already liked
     const index = post.likes.findIndex((id)=> id===String(req.userId))
-    // if the userId is not in the likes data, then like by pushing userId to likes array. else, unlike by removing userId from likes array
     if(index===-1){
         post.likes.push(req.userId)
     }else{
-        // remove userId for unlike
         post.likes.filter((id)=> id!==req.userId)
     }
 
-    //grabs post from above. and remember {new:true} b.c it's an update
     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true } )
     
     res.json(updatedPost);
@@ -90,68 +84,27 @@ export const getPost = async (req, res)=>{
 }
 
 export const commentPost = async (req, res) => {
-    // remember req.params is coming from the id in the url in the api/index.js
     const { id } = req.params
-    // req.body comes from api/index.js as well, but the data that's sent via axios there
     const { value } = req.body
-    // console.log(value)
     // finds the post the comment goes on
     const post = await PostMessage.findById(id)
 
     // goes into the comments for the post and pushes the comment
     post.comments.push(value)
 
-    // update the post so that it contains the new comment, storing the updated post in a variable
     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new:true })
 
     res.json(updatedPost)
 }
 
-
-// update comment
-// export const updateComment = async (req, res) => {
-//     //desctructuring + renaming id to _id for mongoose id
-//     const { id: _id } = req.params;
-
-//     const post = req.body;
-
-//     //check if a post with that id exists
-//     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id, bucko.')
-
-//     //if that post id is valid, then update the post
-//     //note the update data would come from req.body from above (note it's data from frontend). new: true so it gets the updated version of the post.
-//     //also updated findByIdAndUpdate(_id, post, { new: true } to current
-//     const updatedComment = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
-    
-//     res.json(updatedComment)
-// }
-
-
 //get posts. async b.c it takes time to grab the data
 export const getPosts = async (req, res)=>{
     
     try{
-        // const postMessages=await PostMessage.find().sort({$natural:-1});
-
         const postMessages = await PostMessage.find({ users: req.userId })
 
-        //returning postMessages as an array after a 200 OK status
         res.status(200).json(postMessages);
     }catch(error){
         res.status(404).json({message: error.message})
     }
 }
-
-//get posts alt where it's not by userId custom feed but all posts in descending order
-// export const getPosts = async (req, res)=>{
-    
-//     try{
-//         const postMessages=await PostMessage.find().sort({$natural:-1});
-//         // const{ user } = req.params
-
-//         //returning postMessages as an array after a 200 OK status
-//         res.status(200).json(postMessages);
-//     }catch(error){
-//         res.status(404).json({message: error.message})
-//     }
-// }
