64a70b192727ad490c658c272f1d25c6533cd52b
Adam Rathbun <92285612+AdamRathbun@users.noreply.github.com>
2023-06-07 21:04:30 -0400

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,17 @@
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 
-// basically if I want to like a post, I click the like button, the middleware auth checks, then next(), then the like controller, etc.
 
 const auth = async (req, res, next) => {
     dotenv.config()
 
     try {
-        // grabbing token (JWT) from the frontend to check before an user likes, deletes, etc. matches client/src/api/index.js ln9
+        // grabbing token (JWT) from the frontend to check before an user likes, deletes, etc.
         const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null
 
         let decodedData;
 
         if(token){
-            // gets the data from each specific token
             // note the secret has to be the same one used when the token is created in ../controllers/user.js
             decodedData = jwt.verify(token, process.env.JWT_SECRET)
 
